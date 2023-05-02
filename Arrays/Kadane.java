9dddd22aaa2301a7df6483d1517c75f13195e61d
sumitpoonia57 <sumitponia57@gmail.com>
2023-05-03 00:46:05 +0530

    optimized approach

---
+++ Arrays/Kadane.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+import java.util.*;
+
+// write a program to find maximum subarray using kadane algorithm
+public class Kadane {
+    public static void subArrays(int num[]) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < num.length; i++) {
+            sum = sum + num[i];
+            if (sum < 0) {
+                sum = 0;
+            }
+            if (sum > max) {
+                max = sum;
+            }
+        }
+    System.out.println("Maximum Sum:" + max);
+    }
+
+    public static void main(String[] args) {
+        int num[] = { 2, 4, 6, 8, 10, 12 };
+        subArrays(num);
+    }
+}
