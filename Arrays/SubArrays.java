e6d449293e125d002a6e31284af3110bc2c3065b
sumitpoonia57 <sumitponia57@gmail.com>
2023-05-02 21:17:49 +0530

    Arrays

--- Arrays/SubArrays.java
+++ Arrays/SubArrays.java
@@ -1,22 +1,33 @@
 package Arrays;
 // write a program to print subarrays for arrays
-
+import java.util.*;
 public class SubArrays {
     public static void subArrays(int num[]){
+
+        int sum=0;
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
         for(int i=0;i<num.length;i++){
             for(int j=i+1;j<num.length;j++){
                 System.out.print("");
                 for(int k=i;k<j;k++){
                     int remain=num[k];
-                    System.out.print(remain+" ");
-
-
+                    sum+=remain;
+                if(sum>max){
+                    max=sum;
                 }
-          System.out.print(" ");
+                if(sum<min){
+                    min=sum;
+                }
+                    System.out.print(remain+" ");
+                
+                }
+            System.out.println();
             }
-        
             System.out.println();
         }
+        System.out.println("Maximum Sum:"+max);
+        System.out.println("Minimum Sum:"+min);
     }
     public static void main(String[] args) {
         int num[]={2,4,6,8,10,12};
