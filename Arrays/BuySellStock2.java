557f73ca28de1c3f2dcbcca531234ddc1c13f200
sumitpoonia57 <sumitponia57@gmail.com>
2023-05-07 15:04:55 +0530

    arrays

---
+++ Arrays/BuySellStock2.java
@@ -0,0 +1,24 @@
+package Arrays;
+// write a program to calculate buy sell stock2 problem where you caan buy as asmany stocks and sell as any many time  but you cannot buy consecutively two stock
+
+public class BuySellStock2 {
+    public static int buyStock(int num[]) {
+        int profit=0;
+        for(int i=1;i<num.length;i++){
+            if(num[i]>num[i-1]){
+                profit+=(num[i]-num[i-1]);
+
+            }
+
+        }
+        return profit;
+
+    }
+
+    public static void main(String[] args) {
+        int num[] = { 5, 2, 7, 3, 6, 1, 2, 4 };
+        System.out.print(buyStock(num));
+
+    }
+
+}
