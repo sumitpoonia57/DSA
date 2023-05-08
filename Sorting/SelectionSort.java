ac2de9b9494624a4eaf766a0979208c0096c2fe4
sumitpoonia57 <sumitponia57@gmail.com>
2023-05-08 23:00:26 +0530

    updatedSorting

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -3,27 +3,28 @@ package Sorting;
 import java.util.*;
 
 public class SelectionSort {
-
-    public static int[] Selection(int num[]){
+    public static int[] SortS(int num[]){
         int n=num.length;
     // no of turns
-    int temp=Integer.MAX_VALUE;
-    for(int i=0;i<n-2;i++){
-        for(int j=i+1;j<n-1;j++){
-            if(temp>num[j]){
-                temp=num[j];
+
+    for(int i=0;i<=n-1;i++){
+        int min=i;
+        for(int j=i+1;j<=n-1;j++){
+            if(num[j]<num[min]){
+                min=j; 
             }
         }
+        int temp=num[min];
+        num[min]=num[i];
         num[i]=temp;
-        temp=num[i+1];
-        num[i+1]=num[i];
 
     }
         return num;
     }
+
     public static void main(String[] args) {
         int num[]={5,4,1,3,2};
-        System.out.print(Arrays.toString(Selection(num)));
+        System.out.print(Arrays.toString(SortS(num)));
         
     }
     
