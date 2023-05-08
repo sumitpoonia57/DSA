0a76d963998c3efc9f8882a3da59d2544f226dde
sumitpoonia57 <sumitponia57@gmail.com>
2023-05-08 17:11:19 +0530

    updated

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,29 +1,31 @@
 package Sorting;
+
 // write a program to sort array using bubble sort
 import java.util.*;
 
 public class BubbleSort {
-    public static int[] BSort(int num[]){
-        for(int i=0;i<num.length-2;i++){
-            for(int j=0;j<num.length-2-i;j++){
-                if(num[j]>num[j+1]){
-                    int temp=num[j];
-                    num[j]=num[j+1];
-                    num[j+1]=temp;
+    public static int[] BSort(int num[]) {
+        // no of turns=n-2;
+        for (int i = 0; i <= num.length - 2; i++) {
+            for (int j = 0; j <= num.length - 2 - i; j++) {
+                if (num[j] > num[j + 1]) {
+                    int temp = num[j];
+                    num[j] = num[j + 1];
+                    num[j + 1] = temp;
 
                 }
-               
+
             }
         }
 
         return num;
 
     }
+
     public static void main(String[] args) {
-        int num[]={2,4,6,5,8,9};
+        int num[] = { 7, 1, 5, 8, 3, 0, 6 };
         System.out.print(Arrays.toString(BSort(num)));
 
-        
     }
-    
+
 }
