0348bbe1e955dff0ed9dec6730da1c93a986135e
sumitpoonia57 <sumitponia57@gmail.com>
2023-05-13 18:33:28 +0530

    selection sort

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -6,7 +6,6 @@ public class SelectionSort {
     public static int[] SortS(int num[]){
         int n=num.length;
     // no of turns
-
     for(int i=0;i<=n-1;i++){
         int min=i;
         for(int j=i+1;j<=n-1;j++){
@@ -24,8 +23,6 @@ public class SelectionSort {
 
     public static void main(String[] args) {
         int num[]={5,4,1,3,2};
-        System.out.print(Arrays.toString(SortS(num)));
-        
-    }
-    
+        System.out.print(Arrays.toString(SortS(num)));   
+    }  
 }
