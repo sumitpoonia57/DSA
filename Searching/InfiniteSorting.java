0f711f7a2c20ddb5658612746dbac5f5d48c2139
sumitpoonia57 <sumitponia57@gmail.com>
2023-05-15 00:36:52 +0530

    Searching

---
+++ Searching/InfiniteSorting.java
@@ -0,0 +1,35 @@
+package Searching;
+// write a program to sort element from infinte sorted array
+
+
+public class InfiniteSorting {
+    public static int Range(int num[],int key,int low,int high)
+    {
+        low=0;
+        high=1;
+        while(num[high]<key){
+            low=high;
+            high=high*2;
+        }
+         return Sort(num,key,low,high);
+    }
+    public static int Sort(int num[],int key,int si,int ei ){
+          // kaam
+          int mid=(si+ei)/2;
+          if(num[mid]==key){
+              return mid;
+          }
+          if(key>num[mid]){
+             return  Sort(num, key, mid+1, ei);
+          }
+          return Sort(num, key, si, mid-1);
+  
+    }
+    public static void main(String[] args) {
+        int num[]={2,3,4,5,6,7};
+        int key=5;
+        
+        System.out.println(Range(num, 5, 0,1));  
+    }
+    
+}
