0d1fb3d9fae1c845687ada2a566f4cdf2df7fbe7
sumitpoonia57 <sumitponia57@gmail.com>
2023-05-18 00:42:37 +0530

    MajorityElement

---
+++ Arrays/MajorityElement.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class MajorityElement {
+    public static int Majority(int num[]){
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
