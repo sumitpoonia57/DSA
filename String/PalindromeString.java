93dd241f29214d8126d5027f384ab69465b4808c
sumitpoonia57 <sumitponia57@gmail.com>
2023-06-10 14:50:46 +0530

    String

---
+++ String/PalindromeString.java
@@ -0,0 +1,16 @@
+public class PalindromeString {
+    public static void palindrome(String name){
+        String palindrome="";
+        for(int i=name.length()-1;i>=0;i--){
+            char a=name.charAt(i);
+            palindrome=palindrome+a;
+        }
+        System.out.println(name.equals(palindrome));
+    }
+    public static void main(String[] args) {
+        String name="racecar";
+         palindrome(name); 
+        
+    }
+    
+}
