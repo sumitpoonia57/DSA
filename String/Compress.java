8bc9583d55e4e9cafb9b46f844b8abd741df54cc
sumitpoonia57 <sumitponia57@gmail.com>
2023-06-11 00:48:58 +0530

    compress

---
+++ String/Compress.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Compress {
+    // write a program to perfrom string compression
+    // aaabbbcc -> a3b3c2
+    public static String Compression(String str){
+        String newString="";
+        for(int i=0;i<str.length();i++){
+            Integer count=1;
+            while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            newString+=str.charAt(i);
+            if(count>1){
+                newString+=count.toString();
+            }
+        }
+        return newString;
+    }
+    public static void main(String[] args) {
+        String str="aaabbbcc";
+        System.out.print(Compression(str));
+        
+    }
+    
+}
