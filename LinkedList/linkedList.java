c0f4128e113cc417d2c48a04bb2b2802c4ac4770
sumitpoonia57 <sumitponia57@gmail.com>
2023-06-30 00:57:52 +0530

    linkedlist

--- LinkedList/linkedList.java
+++ LinkedList/linkedList.java
@@ -16,10 +16,12 @@ public class linkedList {
     // this is define as propery
         public static Node head;
         public static Node tail;
+        public static int size;
         // in linked list we have to call method by using object like ll so it doesnot matter that your function is static or non static
     public  void addFirst(int data){
             // step1 =create new node
             Node newNode=new Node(data);
+            size++;
             // Base Case
             if(head==null){
                 head=tail=newNode;
@@ -32,6 +34,7 @@ public class linkedList {
         }
         public void addLast(int data){
             Node newNode=new Node(data);
+            size++;
             // Base Case
             // linked list is emppty
             if(head==null){
@@ -56,7 +59,13 @@ public class linkedList {
             System.out.println("null");
         }
         public void add(int indx,int data){
+            // special case if we want add at head
+            if(indx==0){
+                addFirst(data);
+                return;
+            }
             Node newNode=new Node(data);
+            size++;
             Node temp=head;
             int i=0;
             while(i<indx-1){
@@ -67,6 +76,98 @@ public class linkedList {
             newNode.next=temp.next;
             temp.next=newNode;
 
+        }
+        public int removeFirst(){
+            // special case:
+            if(size==0){
+                System.out.println("LL is empty");
+                return Integer.MIN_VALUE;
+            }
+            else if(size==1){
+              int val=head.data;
+              head=tail=null;
+              size=0;
+              return val;
+
+            }
+            int val=head.data;
+           // int val=head.data;
+            head=head.next;
+            size--;
+           // return val;
+           return val;
+        }
+        public int removeLast(){
+            // special case
+              if(size==0){
+                System.out.println("LL is empty");
+                return Integer.MIN_VALUE;
+            }
+             else if(size==1){
+              int val=head.data;
+              head=tail=null;
+              size=0;
+              return val;
+
+            }
+            // prev node i=size-2
+            Node prev=head;
+            for(int i=0;i<size-2;i++){
+              prev=prev.next;
+              System.out.println(prev.data);
+            }
+            int val=tail.data;
+            prev.next=null;
+            size--;
+            return val;
+        }
+        // iterative search
+        public int Search(int key){
+            Node temp=head;
+            int i=0;
+            while(temp!=null){
+                if(temp.data==key){
+                    return i;
+                }
+                temp=temp.next;
+                System.out.println(temp.data);
+                i++;
+            
+            }
+            return -1;
+        }
+        // helper function
+        public int helper(Node head,int key){
+            if(head==null){
+                return -1;
+            }
+            if(head.data==key){
+                return 0;
+            }
+            int indx=helper(head.next,key);
+            if(indx== -1){
+                return -1;
+            }
+            return indx+1;
+
+        }
+        // recursive search
+        public int recSearch(int key){
+            return helper(head, key);
+
+        }
+        // reverse linked list
+        public  void reverse(){
+            Node prev=null;
+            Node current=tail=head;
+            Node next;
+            while(current!=null){
+                next=current.next;
+                current.next=prev;
+                prev=current;
+                current=next;
+            }
+            head=prev;
         }
     public static void main(String[] args) {
         linkedList ll=new linkedList();
@@ -81,6 +182,17 @@ public class linkedList {
         //ll.print();
         ll.add(3,9);
         ll.print();
+       System.out.println(ll.size);
+       ll.removeFirst();
+       ll.print();
+       ll.removeLast();
+       ll.print();
+       System.out.println(ll.Search(3));
+       ll.reverse();
+       ll.print();
+   
+
+       
 
 
         
