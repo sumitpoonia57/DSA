0505c831c52fca7cf8c7c9deb6fd1deaeedc1c33
sumitpoonia57 <sumitponia57@gmail.com>
2023-07-17 11:45:25 +0530

    reverseStack

---
+++ Stack/reverseStack.java
@@ -0,0 +1,35 @@
+package Stack;
+import java.util.*;
+
+public class reverseStack {
+        public static void bottomAtStack(Stack<Integer> stack,int data){
+            // base condition
+            if(stack.isEmpty()){
+                stack.push(data);
+                return;
+           }
+          int top=stack.pop();
+          bottomAtStack(stack, data);
+            stack.push(top);
+        }
+
+    public static void reverse(Stack<Integer> s){
+      if(s.isEmpty()){
+        return;
+      }
+        int top=s.pop();
+        reverse(s);
+        bottomAtStack(s, top);
+    }
+
+
+    public static void main(String[] args) {
+        Stack<Integer> s=new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        reverse(s);
+        System.out.println(Arrays.asList(s));
+        
+    }
+}
