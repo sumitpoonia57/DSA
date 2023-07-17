a5e3b3b0c187e56680a887ceed5f2a675d923b02
sumitpoonia57 <sumitponia57@gmail.com>
2023-07-17 11:17:30 +0530

    reverseString

---
+++ Stack/reverseString.java
@@ -0,0 +1,23 @@
+package Stack;
+import java.util.*;
+
+public class reverseString {
+    public static String toReverse(String str){
+      Stack<Character> s=new Stack<>();
+      int i=0;
+      while(i<str.length()){
+        s.push(str.charAt(i));
+        i++;
+      }
+      StringBuilder sb=new StringBuilder();
+      while(!s.empty()){
+        sb.append(s.pop());
+      }
+      return sb.toString();
+    }
+    public static void main(String[] args) {
+        String str="Sumit";
+        System.out.print(toReverse(str)); 
+    }
+    
+}
