c1c63150473f859397f2480f66183de1a43cde78
sumitpoonia57 <sumitponia57@gmail.com>
2023-07-18 17:12:21 +0530

    DuplicateParenthsis

--- Stack/DuplicateParenthsis.java
+++ Stack/DuplicateParenthsis.java
@@ -6,23 +6,25 @@ public class DuplicateParenthsis {
         Stack<Character> s=new Stack<>();
         for(int i=0;i<str.length();i++){
             char ch=str.charAt(i);
-            // opening case
-            if(ch=='('|| ch=='a' || ch=='+' ||  ch=='b'){
-                s.push(ch);
-            }
             // closing case
-            else{
+            if(ch==')'){
                 int count=0;
-                while(s.peek()==')'){
+                while(s.peek()!='('){
                     s.pop();
                     count++;
                 }
                 if(count<1){
-                return false;
+                return true;
+                 }
+                 else{
+                s.pop();
+                }
             }
+            else{
+                s.push(ch);
             }
         }
-        return true;
+        return false;
     }
     public static void main(String[] args) {
         String str="((a+b)+(c+d))";
