b09ccf8982f0d79d4995d508d6c6a960cdae7e0b
sumitpoonia57 <sumitponia57@gmail.com>
2023-07-18 12:13:14 +0530

    nextGreater

---
+++ Stack/NextGreater.java
@@ -0,0 +1,26 @@
+package Stack;
+import java.util.*;
+// find next greater element of array in new array
+public class NextGreater {
+    public static void main(String[] args) {
+        Stack<Integer> s=new Stack<>();
+        int arr[]={6,8,0,1,3};
+        int next[]=new int[arr.length];
+        for(int i=arr.length-1;i>=0;i--){
+            while(!s.isEmpty() && arr[s.peek()]<arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                next[i]= -1;
+            }
+            else{
+                next[i]=arr[s.peek()];
+               
+            }
+            s.push(i);
+        }
+        System.out.println(Arrays.toString(next));  
+    }
+    
+    
+}
