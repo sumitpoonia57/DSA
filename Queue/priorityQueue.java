ed8489c4d1201ccc8623ca29442743cbaa11461d
sumitpoonia57 <sumitponia57@gmail.com>
2023-07-22 16:57:36 +0530

    cost

--- Queue/priorityQueue.java
+++ Queue/priorityQueue.java
@@ -1,6 +1,6 @@
 package Queue;
 import java.util.*;
-// Prirority Queue // Minimum
+// Prirority Queue // Minimum cost
 // write a program to connect N Ropes with minimum cost
 public class priorityQueue {
     public static void minCost(int a[]){
