3abb0e4fc9379e447f6b0443fe4c0c053adebf69
sumitpoonia57 <sumitponia57@gmail.com>
2023-07-30 15:54:32 +0530

    NextGreater

--- Stack/NextGreater.java
+++ Stack/NextGreater.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class NextGreater {
     public static void main(String[] args) {
         Stack<Integer> s=new Stack<>();
-        int arr[]={6,8,0,1,3};
+        int arr[]={5,2,13,3,8};
         int next[]=new int[arr.length];
         for(int i=arr.length-1;i>=0;i--){
             while(!s.isEmpty() && arr[s.peek()]<arr[i]){
