7ecc6391c062f3003f9ee18db145c7402de8ac6c
sumitpoonia57 <sumitponia57@gmail.com>
2023-08-02 23:48:24 +0530

    CircularQueue

--- Queue/CircularQueue.java
+++ Queue/CircularQueue.java
@@ -1,24 +1,35 @@
 package Queue;
-import java.util.*;
-public  class QueueA {
+
+public class CircularQueue {
     static class Queue{
         static int arr[];
         static int size;
         static int rear;
+        static int front;
         public Queue(int n){
             arr=new int[n];
             rear= -1;
             size=n;
+            front= -1;
         }
         // check if queue is empty or not
         public static boolean isEmpty(){
-            return rear== -1;
+            return rear== -1 && front==-1;
+
+        }
+        // full function
+        public static boolean isFull(){
+            return (rear+1)%size==front;
         }
         // add first
         public static void add(int data){
-            if(rear==size-1){
+            if(isFull()){
                 System.out.println("Queue is Full");
                 return;
+            }
+            // add first element
+            if(front ==-1){
+                front=0;
             }
             rear+=1;
             arr[rear]=data;
@@ -29,35 +40,26 @@ public  class QueueA {
                 System.out.println("Queue is Empty");
                 return -1;
             }
-            int remove=arr[0];
-            for(int i=0;i<rear;i++){
-                arr[i]=arr[i+1];
+            int result=arr[0];
+            //both front and rear at last element of array
+            if(front==rear){
+               front=rear=-1;
+            }else{
+                front=(front+1)%size;
             }
-            rear= rear-1;
-            return remove;
+            return result;
+            // yaha front 1 peeche jayega to time complexity O(1)
         }
         // peek
         public static int peek(){
             if(isEmpty()){
                 System.out.println("Queue is Empty");
             }
-            return arr[0];
+            return arr[front];
         }
     }
     public static void main(String[] args) {
-        Queue q=new Queue(3);
-        q.add(1);
-        q.add(2);
-        q.add(3);
-        System.out.println(q.remove());
-        q.add(4);
-        System.out.println(q.remove());
-         q.add(5);
-        while(!q.isEmpty()){
-            System.out.println(q.peek());
-            q.remove();
-        }
-
+        
         
     }
     
