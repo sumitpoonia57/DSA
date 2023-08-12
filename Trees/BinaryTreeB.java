12a78c2355b2b53dbd13e115845b5b5998b58c7c
sumitpoonia57 <sumitponia57@gmail.com>
2023-08-12 23:49:46 +0530

    InorderTraversal

--- Trees/BinaryTreeB.java
+++ Trees/BinaryTreeB.java
@@ -34,13 +34,21 @@ public class BinaryTreeB {
             preorder(root.left);
             preorder(root.right);
         }
+        public static void inorder(Node root){
+            if(root==null){
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data+" ");
+            inorder(root.right);
+        }
 
     }
     public static  void main(String args[]){
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree=new BinaryTree();
       Node root=  tree.buildTree(nodes);
-      tree.preorder(root);
+      tree.inorder(root);
        
 
     }
