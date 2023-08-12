833af1175038436e3a10156096a8cb5d9c20e2b6
sumitpoonia57 <sumitponia57@gmail.com>
2023-08-12 23:51:22 +0530

    postOrderTraversal

--- Trees/BinaryTreeB.java
+++ Trees/BinaryTreeB.java
@@ -42,13 +42,21 @@ public class BinaryTreeB {
             System.out.print(root.data+" ");
             inorder(root.right);
         }
+        public static void postorder(Node root){
+            if(root==null){
+                return;
+            }
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data+" ");
+        }
 
     }
     public static  void main(String args[]){
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree=new BinaryTree();
       Node root=  tree.buildTree(nodes);
-      tree.inorder(root);
+      tree.postorder(root);
        
 
     }
