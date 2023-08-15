296243f50c2996878d1b1e690e30dad12b561a34
sumitpoonia57 <sumitponia57@gmail.com>
2023-08-15 16:32:48 +0530

    independence

--- Stack/ValidParenthsis.java
+++ Stack/ValidParenthsis.java
@@ -6,7 +6,7 @@
         Stack<Character> s=new Stack<>();
         for(int i=0;i<str.length();i++){
             char ch=str.charAt(i);
-            // check for opening case
+            // c\heck for opening case
             if(ch=='(' || ch=='{'|| ch=='['){
                 s.push(ch);
             }
