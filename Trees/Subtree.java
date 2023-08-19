83aaaf13d6b83b871b4764417b6d6383c991bca0
sumitpoonia57 <sumitponia57@gmail.com>
2023-08-20 00:30:21 +0530

    Subtree

---
+++ Trees/Subtree.java
@@ -0,0 +1,58 @@
+package Trees;
+import java.util.*;
+// write a program to check if subtree is equal to tree subtree or not
+
+public class Subtree {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        public Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    public static boolean isIdentical(Node root,Node subroot){
+        if(root==null && subroot==null){
+            return true;
+        }
+        if(root==null || subroot==null || root.data!=subroot.data){
+            return false;
+        }
+        if(!isIdentical(root.left, subroot.left)){
+            return false;
+        }
+        if(!isIdentical(root.right, subroot.right)){
+            return false;
+        }
+        return true;
+    }
+    public static boolean subtree(Node root,Node subroot){
+        if(root==null){
+            return false;
+        }
+        if(root.data==subroot.data){
+            if(isIdentical(root,subroot)){
+                return true;
+            }
+        }
+        return subtree(root.left,subroot)|| subtree(root.right,subroot);
+    }
+
+    public static void main(String[] args) {
+        Node root=new Node(1);
+        root.left=new Node(2);
+        root.right=new Node(3);
+        root.left.left=new Node(4);
+        root.left.right=new Node(5);
+        root.right.right=new Node(6);
+        Node subroot=new Node(2);
+        subroot.left=new Node(4);
+        subroot.right=new Node(5);
+        System.out.println(subtree(root, subroot));
+
+        
+    }
+    
+}
