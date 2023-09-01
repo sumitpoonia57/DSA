a174808bd42d6a8d33e7dbf5fdfe09025a4e2d17
sumitpoonia57 <sumitponia57@gmail.com>
2023-09-01 23:04:08 +0530

    printInRange

--- BST/Build.java
+++ BST/Build.java
@@ -75,6 +75,22 @@ public class Build {
         }
         return root;
     }
+    public static void printInRange(Node root,int k1,int k2){
+        if(root==null){
+            return;
+        }
+        if(root.data>=k1 && root.data<k2){
+            printInRange(root.left, k1, k2);
+            System.out.println(root.data);
+            printInRange(root.right, k1, k2);
+        }
+         else if(root.data<k1){
+            printInRange(root.right, k1, k2);
+        }
+        else{
+            printInRange(root.left, k1, k2);
+        }
+    }
     public static Node InorderSuccessor(Node root){
         while(root.left!=null){
             root=root.left;
@@ -83,15 +99,16 @@ public class Build {
     }
 
     public static void main(String[] args) {
-        int values[]={5,1,3,4,2,7};
+        int values[]={5,1,3,4,2,6,7};
         Node root=null;
         for(int i=0;i<values.length;i++){
             root=insert(root,values[i]);
         }
         inorder(root);
         System.out.println(Search(root, 1));
-        Delete(root, 1);
-        inorder(root);
+       // Delete(root, 1);
+       // inorder(root);
+        printInRange(root, 5, 7);
     }
     
 }
