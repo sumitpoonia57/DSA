7103f89c069b662fbb0cdb44e9b9b8751a430840
sumitpoonia57 <sumitponia57@gmail.com>
2023-09-01 19:27:03 +0530

    DeleteaNodeinBinaryTree

--- BST/Build.java
+++ BST/Build.java
@@ -48,6 +48,39 @@ public class Build {
              return Search(root.right, val);
         }
     }
+    public static Node Delete(Node root,int val){
+        if(root.data<val){
+            root.right=Delete(root.right, val);
+        }
+        else if(root.data>val){
+            root.left=Delete(root.left, val);
+        }
+        else{
+            // Case 1 Leave node
+            if(root.left==null && root.right==null){
+                return null;
+            }
+            // Case 2 Only One children
+            if(root.left==null){
+                return root.right;
+            }
+            else if(root.right==null){
+                return root.left;
+            }else{
+                // Case 3 when delete node have two children
+                Node IS=InorderSuccessor( root.right);
+                root.data=IS.data;
+                root.right=Delete(root.right, IS.data);
+            }
+        }
+        return root;
+    }
+    public static Node InorderSuccessor(Node root){
+        while(root.left!=null){
+            root=root.left;
+        }
+        return root;
+    }
 
     public static void main(String[] args) {
         int values[]={5,1,3,4,2,7};
@@ -57,6 +90,8 @@ public class Build {
         }
         inorder(root);
         System.out.println(Search(root, 1));
+        Delete(root, 1);
+        inorder(root);
     }
     
 }
