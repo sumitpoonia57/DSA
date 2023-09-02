b89187b53a2a0a574a74dee79bfe3725353a832a
sumitpoonia57 <sumitponia57@gmail.com>
2023-09-02 21:24:32 +0530

    validBinarySearchTree

--- BST/Build.java
+++ BST/Build.java
@@ -112,6 +112,18 @@ public class Build {
         leafPaths(root.right, path);
         path.remove(path.size()-1);
     }
+    public static boolean isValidBST(Node root,Node min,Node max){
+        if(root==null){
+            return true;
+        }
+        if(min!=null && root.data<=min.data){
+            return false;
+        }
+        if(max!=null && root.data>=max.data){
+            return false;
+        }
+        return isValidBST(root.left, min, root) && isValidBST(root.right,root, max);
+    }
 
     public static void main(String[] args) {
         int values[]={8,5,3,6,1,4,10,11,14};
@@ -126,6 +138,7 @@ public class Build {
         //printInRange(root, 5, 12);
         ArrayList<Integer> path=new ArrayList<>();
         leafPaths(root,path);
+       System.out.println(isValidBST(root, null, null)); 
     }
     
 }
