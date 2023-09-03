85acead39c88d2294bd11eb54fe6752cd083fc67
sumitpoonia57 <sumitponia57@gmail.com>
2023-09-03 22:24:20 +0530

    CeilValueoFBST

--- BST/Floor.java
+++ BST/Floor.java
@@ -26,6 +26,21 @@ public class Floor {
         }
         return ans;
     }
+    public static int Ceil(Node root,int key){
+        int ans=Integer.MIN_VALUE;
+        while(root!=null){
+            if(root.data==key){
+                return root.data;
+            }
+            if(root.data>key){
+                ans=root.data;
+                root=root.left;
+            }else{
+                root=root.right;
+            }
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         Node root = new Node(8);
         root.left = new Node(4);
@@ -35,6 +50,7 @@ public class Floor {
         root.right.left = new Node(10);
         root.right.right = new Node(14);
         System.out.println(Floor(root, 11));
+        System.out.println(Ceil(root, 11));
     }
     
 }
