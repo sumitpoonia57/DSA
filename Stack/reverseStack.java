28138a4f647fed4e93404f143403d35e740e7a44
sumitpoonia57 <sumitponia57@gmail.com>
2023-09-06 16:16:27 +0530

    reversestack

--- Stack/reverseStack.java
+++ Stack/reverseStack.java
@@ -1,6 +1,6 @@
 package Stack;
 import java.util.*;
-//Reve
+//Reversestack
 public class reverseStack {
         public static void bottomAtStack(Stack<Integer> stack,int data){
             // base condition
