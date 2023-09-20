82a7329e8b85258e29de2c200de2977cd6c29b48
sumitpoonia57 <sumitponia57@gmail.com>
2023-09-20 15:58:23 +0530

    Parenthsis

--- Stack/DuplicateParenthsis.java
+++ Stack/DuplicateParenthsis.java
@@ -1,6 +1,6 @@
 package Stack;
 import java.util.*;
-//Duplic
+//DuplicateParenthsis
 public class DuplicateParenthsis {
     public static boolean duplicate(String str){
         Stack<Character> s=new Stack<>();
