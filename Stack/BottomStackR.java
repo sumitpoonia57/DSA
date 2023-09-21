d38bf02262dc143544046872b05f6e0a7c311110
sumitpoonia57 <sumitponia57@gmail.com>
2023-09-21 15:55:53 +0530

    BottomDS

--- Stack/BottomStackR.java
+++ Stack/BottomStackR.java
@@ -14,6 +14,7 @@ public class BottomStackR {
     }
 
     public static void main(String[] args) {
+        // Input
         Stack<Integer> stack=new Stack<>();
         stack.push(1);
         stack.push(2);
