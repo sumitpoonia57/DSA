2c8fd2f86a95611f1eec658e8a931d7dd6de44df
sumitpoonia57 <sumitponia57@gmail.com>
2023-10-08 15:25:25 +0530

    Pattern2

--- Pattern/Pattern2.java
+++ Pattern/Pattern2.java
@@ -1,6 +1,6 @@
 public class Pattern2 {
     public static void main(String[] args) {
-        // Print Number Pattern
+        // Print NumberPattern
         int n = 5;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
