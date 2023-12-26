faaf4d0f091bf17cf8f798884b478dabd0d19124
sumitpoonia57 <sumitponia57@gmail.com>
2023-12-27 00:09:16 +0530

    ClimbingStairsRecursive

---
+++ DynamicProgramming/ClimbingStairs.java
@@ -0,0 +1,21 @@
+package DynamicProgramming;
+// write a program too solve climbing stair problem
+public class ClimbingStairs {
+    // Recursive approach
+    public static int ways(int n){
+        if(n==0){
+            return 1;
+        }
+        if(n<0){
+            return 0;
+        }
+        return ways(n-1)+ways(n-2);
+
+    }
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(ways(n));
+        
+    }
+    
+}
