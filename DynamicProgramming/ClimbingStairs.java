4ae3a527aad23f41410f146865aba2bc612ef9f7
sumitpoonia57 <sumitponia57@gmail.com>
2023-12-27 01:28:13 +0530

    TabulationOfClimbingStairs

--- DynamicProgramming/ClimbingStairs.java
+++ DynamicProgramming/ClimbingStairs.java
@@ -26,11 +26,24 @@ public class ClimbingStairs {
         }
        return  dp[n]=waysmemo(n-1, dp)+waysmemo(n-2, dp);
     }
+    // Tabulation
+    public static int waystebo(int n){
+      int dp[]=new int[n+1];
+      dp[0]=1;
+      for(int i=1;i<=n;i++){
+        if(i==1){
+            dp[i]=dp[i-1]+0;
+        }else{
+        dp[i]=dp[i-1]+dp[i-2];
+        }
+      }
+      return dp[n];
+    }
     public static void main(String[] args) {
         int n=5;
         int dp[]=new int[n+1];
         Arrays.fill(dp,-1);
-        System.out.println(waysmemo(n,dp));
+        System.out.println(waystebo(n));
         
     }
     
