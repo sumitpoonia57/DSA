1bcc729e45c46e607946f84f3ac1c3af1e4b3f73
sumitpoonia57 <sumitponia57@gmail.com>
2023-12-26 18:48:07 +0530

    Tabulation

--- DynamicProgramming/Fibonacci.java
+++ DynamicProgramming/Fibonacci.java
@@ -1,6 +1,7 @@
 package DynamicProgramming;
 //Write a program to caalculate fibonacci series of number using optimized way
 public class Fibonacci {
+    // Memoization 
     public static int fibo(int n,int arr[]){
         if(n==0 || n==1){
             return n;
@@ -11,10 +12,19 @@ public class Fibonacci {
         arr[n]=fibo(n-1,arr)+fibo(n-2,arr);
         return arr[n];
     }
+    // Tabulation to solve fibonacci series question
+    public static int fibotabulation(int n){
+        int dp[]=new int[n+1];
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
     public static void main(String[] args) {
         int n=6;
         int arr[]=new int[n+1];
-    System.out.println(fibo(n,arr));
+    System.out.println(fibotabulation(n));
     }
     
 }
